49162fc8dd817d450b0bbe84e43dcae196a8bec1
11061969 <contact@freitdomo.com>
2026-09-12 18:21:16 +0700

    Add RootLayout component with metadata for app

---
+++ app/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Freitdomo",
+  description: "Simulateur de recettes de crèmes glacées, sorbets et vegan",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
