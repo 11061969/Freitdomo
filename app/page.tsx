36c6bef3e5824211e55a47b1f277272e2d51960c
11061969 <contact@freitdomo.com>
2026-09-14 15:38:56 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>Freitdomo</h1>
-      <p>Application de formulation de crèmes glacées, sorbets et vegan.</p>
-      <p>Version de démarrage</p>
-    </main>
-  )
-}
