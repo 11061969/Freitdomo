250b926c6c2dbc8627fb85fb4cafe46ef3862276
11061969 <contact@freitdomo.com>
2026-09-25 08:15:38 +0700

    Refactor layout by removing redundant div
    Removed unnecessary div wrapping density and related cards.

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -427,7 +427,7 @@ export default function RecipeDetailPage() {
             </>
           )}
 
-                         <h2 style={{ fontSize: 20, marginBottom: 12 }}>Structure et Texture</h2>
+                                   <h2 style={{ fontSize: 20, marginBottom: 12 }}>Structure et Texture</h2>
           <div
             style={{
               display: "flex",
@@ -456,20 +456,6 @@ export default function RecipeDetailPage() {
             {renderCard("Masse molaire stabi", calcs.molarMassStabi, "", "molarMassStabi", 0)}
             {renderCard("Saturation", calcs.saturation, "%", "saturation")}
             {renderCard("Point de congelation", calcs.freezingPoint, "C", "freezingPoint")}
-          </div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 12,
-              alignItems: "flex-start",
-            }}
-          >
-            {renderCard("Densite", calcs.density, "", "density", 3)}
-            {!isSorbet && renderCard("MG solide", calcs.mgSolide, "%", "mgSolide")}
-            {renderCard("Masse molaire stabi", calcs.molarMassStabi, "", "molarMassStabi", 0)}
-            {renderCard("Saturation", calcs.saturation, "%", "saturation")}
           </div>
         </div>
       )}
